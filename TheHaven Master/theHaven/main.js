196a8111ce79854986b15c79361e8ed8d519c12e
Connor <connorclifford17@gmail.com>
2019-11-25 19:12:14 -0500

    Delete main.js

--- TheHaven Master/theHaven/main.js
+++
@@ -1,9 +0,0 @@
-//------------------moblie top navigation------------------//
-function mobileMenu() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
